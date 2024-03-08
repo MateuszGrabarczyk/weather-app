82f4524597969188503c60f8bc0336d800bfcd12
MateuszGrabarczyk <stepowa28@gmail.com>
2024-03-08 12:06:47 +0100

    ✨Add header for day of the week and date

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@
 import Navbar from "@/components/Navbar";
 import { useQuery } from "react-query";
 import axios from "axios";
+import { format, fromUnixTime, parseISO } from "date-fns";
 
 interface WeatherDetail {
   dt: number;
@@ -70,6 +71,8 @@ export default function Home() {
     }
   );
 
+  const firstData = data?.list[0];
+
   if (isLoading) {
     return (
       <div className="flex items-center min-h-screen justify-center">
@@ -81,6 +84,19 @@ export default function Home() {
   return (
     <div className="flex flex-col gap-4 bg-gray-100 min-h-screen">
       <Navbar />
+      <main className="px-3 max-w-7xl mx-auto flex flex-col hap-9 w-full pb-10 pt-4">
+        <section>
+          <div>
+            <h2 className="flex gap-1 text-2xl items-end">
+              <p>{format(parseISO(firstData?.dt_txt ?? ""), "EEEE")}</p>
+              <p className="text-lg">
+                ({format(parseISO(firstData?.dt_txt ?? ""), "dd.MM.yyyy")})
+              </p>
+            </h2>
+          </div>
+        </section>
+        <section></section>
+      </main>
     </div>
   );
 }
